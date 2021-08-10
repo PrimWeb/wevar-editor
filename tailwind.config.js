6416c6954040f5b4fd622e8aa3155a3c5e30045e
DanP <danp@primweb.cz>
2021-08-10 22:31:08 +0200

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2680eb',
+        'dar-gray': '#4b4b4b',
+        'light-gray-0': '#eaeaea',
+        'light-gray-1': '#4b4b4b',
+        'light-gray-2': '#808080',
+        'renderer-gray': '#e0e0e0',
+        red: '#e34850',
+        'green-400': '#2d9d78',
+        'green-500': '#268e6c',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
